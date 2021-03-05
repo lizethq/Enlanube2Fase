23245b3d0f81d0153a6d1c2cca4c387ebedb7ae7
FelipeNavasTodoo <ln@todoo.co>
2021-03-05 10:27:12 -0500

    Initial module commit

---
+++ display_traceability_messages/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': 'Date and time display in traceability messages',
+
+    'version': '13.1',
+
+    'author': "Todoo SAS",
+
+    'contributors': ['Luis Felipe Navas ln@todoo.co'],
+
+    'website': "www.todoo.co",
+
+    'category': 'Reports',
+
+    'depends': [
+
+        'account_accountant',
+        'account',
+
+    ],
+
+    'data': [       
+        'views/views.xml',
+    ],
+    'installable': True
+}
