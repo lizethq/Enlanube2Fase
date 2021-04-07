9f862d813fb56f433a869fd90dae04649b010c1c
FelipeNavasTodoo <ln@todoo.co>
2021-04-07 16:38:36 -0500

    dos

--- sequence_helpdesk_ticket/models/sequence.py
+++ sequence_helpdesk_ticket/models/sequence.py
@@ -2,12 +2,13 @@ from odoo import models, fields, api
 
 class HelpdeskTicket(models.Model):
     _inherit = 'helpdesk.ticket'
-    sequence = fields.Char(copy=False, string='Secuencia Tickets', invisible=True)
-    
+    sequence = fields.Char(string="Secuencia Tickets")
+   
     _rec_name = 'sequence'
 
     @api.model
-    def create(self, vals):
-        vals['sequence'] = self.env['ir.sequence'].next_by_code('helpdesk.ticket')
-        result = super(HelpdeskTicket, self).create(vals)
-        return result
+    def create(self, vals):    	
+    	vals['sequence'] = self.env['ir.sequence'].next_by_code('sequence.ticket')
+
+    	result = super(HelpdeskTicket, self).create(vals)
+    	return result
