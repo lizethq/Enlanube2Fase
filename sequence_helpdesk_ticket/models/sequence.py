78419fdd2b6833a432a6e3cd32fcfc1b3cef1862
FelipeNavasTodoo <ln@todoo.co>
2021-04-07 22:28:22 -0500

    nuevo campo

--- sequence_helpdesk_ticket/models/sequence.py
+++ sequence_helpdesk_ticket/models/sequence.py
@@ -5,10 +5,10 @@ class HelpdeskTicket(models.Model):
     _rec_name = 'sequence'
 
     sequence = fields.Char(string="Secuencia Tickets")
+    display_name = fields.Char("Display Name", related="sla_status_ids.ticket_id",store=True)
    
     @api.model
     def create(self, vals):    	
     	vals['sequence'] = self.env['ir.sequence'].next_by_code('sequence.ticket')
-
     	result = super(HelpdeskTicket, self).create(vals)
     	return result
