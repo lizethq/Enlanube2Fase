91942a5c78a066b3d493d960eb03721d8be33ccb
FelipeNavasTodoo <ln@todoo.co>
2021-04-09 11:37:54 -0500

    new module

---
+++ display_of_start_end_time/__manifest__.py
@@ -0,0 +1,40 @@
+###############################################################################################
+#
+# Luis Felipe Paternina VitaL
+# lfpaternina93@gmail.com                                
+# 
+# 
+# Cel: +573215062353
+#
+#
+# Bogotá,Colombia
+#
+#
+#
+###############################################################################################
+{
+    'name': 'custom module LFPV',
+
+    'version': '13.0',
+
+    'author': "Luis Felipe Paternina",
+
+    'contributors': ['Luis Felipe Paternina lp@todoo.co'],
+
+    'website': "",
+
+    'category': 'reports',
+
+    'depends': [
+
+        'project',
+        
+    ],
+
+    'data': [
+        
+        'views/project.xml',
+       
+    ],
+    'installable': True
+}
---
+++ display_of_start_end_time/models/project.py
@@ -0,0 +1,11 @@
+from odoo import fields, models, api, exceptions
+import re
+from odoo.exceptions import ValidationError
+from datetime import datetime, date, timedelta
+
+class Project(models.Model):
+    _inherit = "project.task"
+
+    _date_start = fields.Datetime(string='Fecha inicio', index=True, copy=False)
+    _date_end = fields.Datetime(string='Fecha final', index=True, copy=False)
+
